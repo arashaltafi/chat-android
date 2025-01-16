008c331a656d83e614771fb0a5240618a8ea7f9d
Arash Altafi <arashaltafi1377@gmail.com>
2025-01-16 22:43:15 +0330

    fix ui

--- app/src/main/java/com/arash/altafi/chatandroid/utils/Constance.kt
+++ app/src/main/java/com/arash/altafi/chatandroid/utils/Constance.kt
@@ -3,20 +3,42 @@ package com.arash.altafi.chatandroid.utils
 object Constance {
     const val UNAUTHORIZED = "401"
     const val ERROR = "error"
+
     const val SEND_MESSAGE = "sendMessage"
     const val GET_MESSAGES = "getMessages"
     const val RECEIVE_MESSAGE = "receiveMessage"
+    const val SEEN_MESSAGE = "seenMessage"
+    const val REACTION_MESSAGE = "reactionMessage"
+
     const val INTRODUCE = "introduce"
     const val LOGOUT = "logout"
     const val LOGIN = "login"
     const val REGISTER = "register"
     const val VERIFY = "verify"
+
     const val USERS = "getUsers"
-    const val DIALOGS = "getDialogs"
+
     const val EDIT_PROFILE = "editProfile"
+    const val EDIT_AVATAR_PROFILE = "editProfileAvatar"
+    const val PROFILE_CHANGE = "profileChange"
+    const val USER_INFO = "userInfo"
+
     const val GET_CHAT_ROOM = "getMessagesChatRoom"
     const val USER_STATUS_CHAT_ROOM = "userStatusChatRoom"
     const val RECEIVE_MESSAGE_CHAT_ROOM = "receiveMessageChatRoom"
     const val SEND_CHAT_ROOM = "sendMessageChatRoom"
-    const val USER_INFO = "userInfo"
+
+    const val BLOCK_BY_ME = "blockByMe"
+    const val UNBLOCK_BY_ME = "unBlockByMe"
+    const val BLOCK = "block"
+    const val UNBLOCK = "unBlock"
+
+    const val DELETE_DIALOG = "deleteDialog"
+    const val DIALOGS = "getDialogs"
+    const val CLEAR_HISTORY = "clearHistory"
+    const val UPDATE_DIALOG = "updateDialog"
+
+    const val START_TYPING = "startTyping"
+    const val STOP_TYPING = "stopTyping"
+    const val IS_TYPING = "isTyping"
 }
