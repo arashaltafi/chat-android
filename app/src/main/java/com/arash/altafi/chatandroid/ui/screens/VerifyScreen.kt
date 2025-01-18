516a1ac0c41a1e4cbeac2948ffe386de98e1c4ac
Arash Altafi <arashaltafi1377@gmail.com>
2025-01-18 12:53:18 +0330

    fix otp verify

--- app/src/main/java/com/arash/altafi/chatandroid/ui/screens/VerifyScreen.kt
+++ app/src/main/java/com/arash/altafi/chatandroid/ui/screens/VerifyScreen.kt
@@ -1,39 +1,31 @@
 package com.arash.altafi.chatandroid.ui.screens
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.text.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Error
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.focus.FocusRequester
-import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.*
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardCapitalization
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.VisualTransformation
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.arash.altafi.chatandroid.R
 import com.arash.altafi.chatandroid.data.model.UserInfoModel
 import com.arash.altafi.chatandroid.ui.components.NetworkConnectivityListener
+import com.arash.altafi.chatandroid.ui.components.OTPInputTextFields
 import com.arash.altafi.chatandroid.ui.navigation.Route
 import com.arash.altafi.chatandroid.ui.theme.CustomFont
 import com.arash.altafi.chatandroid.viewmodel.AuthViewModel
@@ -65,19 +57,22 @@ fun VerifyScreen(navController: NavController, mobile: String) {
 
     val context = LocalContext.current
 
-    val focusRequester = remember { FocusRequester() }
-    var code by remember { mutableStateOf("") }
-
     val keyboardController = LocalSoftwareKeyboardController.current
     val focusManager = LocalFocusManager.current
 
+    val otpValues = remember { mutableStateListOf("", "", "", "") }
+
     LaunchedEffect(Unit) {
-        focusRequester.requestFocus()
         keyboardController?.show()
     }
 
+    var hasErrorServer by remember { mutableStateOf(false) }
+
     LaunchedEffect(liveVerify) {
         liveVerify?.message?.let {
+            if (it.contains("خوش آمدید").not()) {
+                hasErrorServer = true
+            }
             Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
         }
         liveVerify?.token?.let {
@@ -144,78 +139,37 @@ fun VerifyScreen(navController: NavController, mobile: String) {
                 verticalArrangement = Arrangement.Center,
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                CompositionLocalProvider(LocalLayoutDirection provides LayoutDirection.Ltr) {
-                    OutlinedTextField(
-                        modifier = Modifier
-                            .padding(top = 20.dp)
-                            .width(280.dp)
-                            .focusRequester(focusRequester),
-                        textStyle = TextStyle(
-                            textAlign = TextAlign.Start,
-                            fontFamily = CustomFont,
-                        ),
-                        value = code,
-                        onValueChange = { newValue ->
-                            if (newValue.length <= 4) code = newValue
-                        },
-                        label = {
-                            Text(
-                                text = context.getString(R.string.otp),
-                                fontSize = 12.sp,
-                                fontFamily = CustomFont,
-                                color = Color.White
-                            )
-                        },
-                        placeholder = {
-                            Text(
-                                text = "1234",
-                                color = Color.White,
-                                modifier = Modifier.fillMaxWidth(),
-                                style = TextStyle(
-                                    textAlign = TextAlign.Start
-                                ),
-                                fontFamily = CustomFont,
-                            )
-                        },
-                        singleLine = true,
-                        enabled = true,
-                        visualTransformation = VisualTransformation.None,
-                        trailingIcon = {
-                            Icon(
-                                painter = painterResource(R.drawable.round_mobile_screen_share_24),
-                                contentDescription = context.getString(R.string.otp),
-                                tint = Color.White
-                            )
-                        },
-                        keyboardOptions = KeyboardOptions(
-                            capitalization = KeyboardCapitalization.None,
-                            keyboardType = KeyboardType.Number,
-                            imeAction = ImeAction.Done,
-                            autoCorrect = false,
-                        ),
-                        keyboardActions = KeyboardActions(
-                            onDone = {
-                                keyboardController?.hide()
-                                focusManager.clearFocus()
-                                authViewModel.sendVerify(
-                                    code = code,
-                                    phone = mobile
-                                )
-                            }
+                OTPInputTextFields(
+                    modifier = Modifier
+                        .width(280.dp),
+                    otpValues = otpValues,
+                    otpLength = 4,
+                    onOtpInputComplete = {
+                        keyboardController?.hide()
+                        focusManager.clearFocus()
+                        authViewModel.sendVerify(
+                            code = otpValues.joinToString(""),
+                            phone = mobile
                         )
-                    )
-                }
+                    },
+                    onUpdateOtpValuesByIndex = { index, value ->
+                        otpValues[index] = value
+                    },
+                    isError = hasErrorServer
+                )
 
                 Spacer(modifier = Modifier.height(20.dp))
 
                 Button(
                     onClick = {
                         authViewModel.sendVerify(
-                            code = code,
+                            code = otpValues.joinToString(""),
                             phone = mobile
                         )
                     },
-                    modifier = Modifier.width(280.dp).imePadding(),
+                    modifier = Modifier
+                        .width(280.dp)
+                        .imePadding(),
                     colors = ButtonDefaults.buttonColors(containerColor = Color.White)
                 ) {
                     Text(
