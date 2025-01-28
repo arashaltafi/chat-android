b0f43e4b847e37f87d8a990317dd0b14a30e9bc2
Arash Altafi <arashaltafi1377@gmail.com>
2025-01-28 22:16:35 +0330

    change splash ui

--- app/src/main/java/com/arash/altafi/chatandroid/ui/screens/SplashScreen.kt
+++ app/src/main/java/com/arash/altafi/chatandroid/ui/screens/SplashScreen.kt
@@ -1,27 +1,37 @@
 package com.arash.altafi.chatandroid.ui.screens
 
-import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.*
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.*
 import kotlinx.coroutines.delay
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import com.arash.altafi.chatandroid.ui.navigation.Route
 import com.arash.altafi.chatandroid.viewmodel.DataStoreViewModel
+import com.arash.altafi.chatandroid.R
+import com.arash.altafi.chatandroid.ui.components.LottieComponent
+import com.arash.altafi.chatandroid.ui.theme.CustomFont
 
 @Composable
 fun SplashScreen(navController: NavController) {
+    val context = LocalContext.current
     val dataStoreViewModel: DataStoreViewModel = hiltViewModel()
     val token by dataStoreViewModel.cachedToken.observeAsState()
+
+    val versionName =
+        context.packageManager.getPackageInfo(context.packageName, 0).versionName ?: "1"
 
     LaunchedEffect(Unit) {
         dataStoreViewModel.getToken()
@@ -39,14 +49,39 @@ fun SplashScreen(navController: NavController) {
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.primary),
+            .padding(horizontal = 32.dp, vertical = 48.dp),
         contentAlignment = Alignment.Center
     ) {
-        Text(
-            text = "Splash Screen",
-            color = Color.White,
-            fontSize = 32.sp,
-            fontWeight = FontWeight.Bold
-        )
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = context.getString(R.string.app_name),
+                color = Color.White,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = CustomFont,
+            )
+
+            Box(
+                modifier = Modifier.weight(1f),
+                contentAlignment = Alignment.Center
+            ) {
+                LottieComponent(
+                    size = DpSize(width = 200.dp, height = 200.dp),
+                    loop = true,
+                    lottieFile = R.raw.chat_3
+                )
+            }
+
+            Text(
+                text = versionName,
+                color = Color.White,
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold,
+                fontFamily = CustomFont
+            )
+        }
     }
 }
